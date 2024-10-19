3ed0f7fafe9b8f5911e1a7f3e05a55886e52590a
CWilliams-2 <cliftonwilliamsjr24@gmail.com>
2024-10-19 16:19:25 -0700

    Created main menu and options menu. Options menu has volume sliders.

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,1 +1,5 @@
 extends Node2D
+
+func _physics_process(delta: float) -> void:
+	if Input.is_action_just_released("pause"):
+		get_tree().change_scene_to_file("res://scenes/menus/main_menu.tscn")
--- scripts/menus/main_menu.gd
+++ scripts/menus/main_menu.gd
@@ -5,7 +5,7 @@ func _on_play_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/game.tscn")
 
 func _on_options_pressed() -> void:
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://scenes/menus/options_menu.tscn")
 
 # When quit button is pressed, quit game
 func _on_exit_pressed() -> void:
