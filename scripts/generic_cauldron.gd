213df9ea64854eb0cbc408b67cca7ff6b2083e62
CWilliams-2 <cliftonwilliamsjr24@gmail.com>
2024-10-19 14:01:35 -0700

    Added comments to cauldron script

--- scripts/generic_cauldron.gd
+++ scripts/generic_cauldron.gd
@@ -7,6 +7,7 @@ var brewing = false
 func _ready():
 	add_to_group("cauldrons")
 
+# When cauldron is clicked, brew potion
 func _on_area_2d_input_event(_viewport: Node, _event: InputEvent, _shape_idx: int) -> void:
 	if Input.is_action_just_pressed("click"):
 		brewing = true
@@ -14,17 +15,19 @@ func _on_area_2d_input_event(_viewport: Node, _event: InputEvent, _shape_idx: in
 func _physics_process(delta: float) -> void:
 	if brewing:
 		var sum = 0
-		for i in ingredient_list:
+		for i in ingredient_list:	# Add ingredient ids together
 			sum += i
-		potion_id = sum
+		potion_id = sum	# Potion id = sum of ingredient ids
 		print(potion_id)
-		ingredient_list=[]
+		ingredient_list=[]	# Reset ingredient_list
 		brewing = false
 
+# Function for adding ingredient 1
 func _on_ingredient_add_ingredient(id) -> void:
 	ingredient_list.append(id)
 	print(ingredient_list)
 
+# Function for adding ingredient 2
 func _on_ingredient_2_add_ingredient(id) -> void:
 	ingredient_list.append(id)
 	print(ingredient_list)
