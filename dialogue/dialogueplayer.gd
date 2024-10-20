25fa3db5227fd1a5b3eeb08b1189af2654095e23
Alyssa Barrientos <aly3barr@gmail.com>
2024-10-20 05:09:47 -0700

    Adding to Dialogue
    - add to more

--- dialogue/dialogueplayer.gd
+++ dialogue/dialogueplayer.gd
@@ -10,34 +10,46 @@ var D_Active = false
 
 func _ready():
 	$NinePatchRect.visible = false
-	
+	await get_tree().create_timer(5).timeout
+	$NinePatchRect.visible = true
+	Start()
+
 func Start():
 	if D_Active:
 		return
 	D_Active = true
-	$NinePatchRect.vidsible = true
+	print("Starting dialogue...")
+	$NinePatchRect.visible = true  # Fix typo
 	Dialogue = Load_Dialogue()
+	print("Dialogue loaded: ", Dialogue)
 	Current_Dialogue_ID = -1
 	Next_Script()
-		
+
 func Load_Dialogue():
 	var File = FileAccess.open("res://dialogue/Customer_dialouge1.json", FileAccess.READ)
-	var Content = JSON.parse_string(File.get_as_text())
-	return Content
-	
+	var Content = File.get_as_text()
+	var Parsed_Content = JSON.parse_string(Content)
+	if Parsed_Content.error != OK:
+		print("Error parsing dialogue file: ", Parsed_Content.error)
+		return []
+	return Parsed_Content.result
+
 func _input(event):
 	if !D_Active:
 		return
 	if event.is_action_pressed("ui_accept"):
 		Next_Script()
-		
+
 func Next_Script():
 	Current_Dialogue_ID += 1
+	print("Current Dialogue ID: ", Current_Dialogue_ID)
 	if Current_Dialogue_ID >= len(Dialogue):
 		D_Active = false
 		$NinePatchRect.visible = false
-		emit_signal("Dialogue_Finished") 
+		emit_signal("Dialogue_Finished")
+		print("Dialogue finished.")
 		return
-		
+
 	$NinePatchRect/Name.text = Dialogue[Current_Dialogue_ID]['name']
 	$NinePatchRect/Text.text = Dialogue[Current_Dialogue_ID]['text']
+	print("Showing dialogue: ", Dialogue[Current_Dialogue_ID]['text'])
--- dialogue/singletons/customer_order.gd
+++ dialogue/singletons/customer_order.gd
@@ -17,9 +17,6 @@ var Customer_Potion_Request = 0
 #unc _ready():
 #$CustomerArea.connect("potion_received", self, "_on_potion_received")
 
-func start_dialogue():
-	return
-
 func _on_potion_received(potion_type):
 	if Order1_Active:
 		if potion_type == Potion1:  # Assuming Potion1 is set earlier
@@ -37,17 +34,15 @@ func Countdown_timer():
 
 func _process(delta):
 	label.text = "%02d:%02d" % Countdown_timer()
-	if Order1_Active:
-		Check_Potion_Order()
-		if Potion1 <= 1:
-			print("Order1 Completed")
-			Order1_Active = false
-			Order1_Completed = true
-			# If the timer is up
-		if timer.time_left <= 0:
-			Order1_Active = false
-			print("Order1 failed due to timeout.")
-			emit_signal("Order1_Made")
+	if Order1_Active == true:
+		print("Order1 Completed")
+		Order1_Active = false
+		Order1_Completed = true
+		# If the timer is up
+	if timer.time_left <= 0:
+		Order1_Active = false
+		print("Order1 failed due to timeout.")
+		emit_signal("Order1_Made")
 	#if Order2_Active():
 
 func Order1_Chat():
@@ -64,25 +59,6 @@ func Next_Order() -> void:
 func _on_take_order_button_1_pressed() -> void:
 	#$Order1_ui.visible = false #close order ui
 	timer.start()  # Start the timer here
+	$Order1_ui.visible = false
 	Order1_Active = true
-	Potion1 = 0 #is potion in inventory
-	emit_signal("Order1_Made")
-	
-func Check_Potion_Order():
-	Customer_Potion_Request = Get_Customer_Potion_Request()
- 
-	if Customer_Potion_Request == Potion1:
-		Money += 10  # Increase money if they match
-		print("Potion delivered successfully! Current money: ", Money)
-	else:
-		Money -= 5  # Decrease money if they don't match
-		print("Potion mismatch! Current money: ", Money)
-
-func Get_Customer_Potion_Request():
-	if Potion1:
-	# This is a placeholder function. Replace it with your actual logic to get the requested potion type.
-		return 0  # Replace with actual customer request
-	
-
-	
-	
+	emit_signal("Order_Log_Closed")
